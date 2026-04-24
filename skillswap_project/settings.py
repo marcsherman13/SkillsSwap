c6f0fc8524d45d5261e574c8c4622195eb05f627
Marc Sherman <marcalansherman@gmail.com>
2026-04-24 18:44:27 -0500

    Fix deployment: add Gunicorn, Procfile, and production settings

--- skillswap_project/settings.py
+++ skillswap_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vcpam@aoy24y^#na4u!853%zr&m@w1s4*-c55)zs0)uc!)vapk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
